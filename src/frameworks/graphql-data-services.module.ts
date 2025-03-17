bb28dbb6a918dac136dc007305709cfaa2ea5707
GayanFortude <GayanB@fortude.co>
2025-03-17 16:18:55 +0530

    task-01-gayan-2024-013-17-03

--- src/frameworks/graphql-data-services.module.ts
+++ src/frameworks/graphql-data-services.module.ts
@@ -4,10 +4,17 @@ import { PostGrayDataServices } from './graphql-data-services.service';
 import { IDataServices } from 'src/core/abstracts/data-service.abstract';
 import { DATA_BASE_CONFIGURATION } from 'src/configeration';
 import { Course } from 'src/core';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+    }),
     TypeOrmModule.forRoot(DATA_BASE_CONFIGURATION),
     TypeOrmModule.forFeature([Course]),
   ],
--- src/services/data-services.module/data-services.module.ts
+++ src/services/data-services.module/data-services.module.ts
@@ -1,17 +1,8 @@
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { PostGrayDataServicesModule } from 'src/frameworks/graphql-data-services.module';
 
 @Module({
-  imports: [PostGrayDataServicesModule,
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      playground: true,
-    }),
-  ],
+  imports: [PostGrayDataServicesModule],
   exports: [PostGrayDataServicesModule],
 })
 export class DataServicesModule {}
